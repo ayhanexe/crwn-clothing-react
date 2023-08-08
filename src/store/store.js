bf3ab37f76901050481e65df00ff3cb2104d00a6
0x21106.js <aykhansab@code.edu.az>
2023-08-08 11:33:01 +0400

    cart added to whitelist in store for persist data with local storage

--- src/store/store.js
+++ src/store/store.js
@@ -21,7 +21,7 @@ const composedMiddlewares = composeEnhancer(applyMiddleware(...middlewares));
 const persistConfig = {
   key: "root",
   storage,
-  blacklist: ["user"],
+  whitelist: ["cart"],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
